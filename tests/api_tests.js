1f9a9507e02412e88752b14319077292a3bc655d
VadimNastoyashchy <Vadim.89@ukr.net>
2025-06-17 10:59:54 +0300

    Update api example with new funct.

--- tests/api_tests.js
+++ tests/api_tests.js
@@ -1,10 +1,91 @@
-import { describe, test, expect } from "scripterio";
+import {
+  describe,
+  test,
+  expect,
+  request,
+  beforeEach,
+  afterAll,
+} from "scripterio";
 
-describe("API tests:", () => {
-  test("[GET] - pokeapi returns correct pokemon", { timeout: 20_000 }, async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+const BASE_ENDPOINT = "https://api.restful-api.dev/objects";
+let preparedData;
+
+const cleanData = async (id) => {
+  return await request.delate(`${BASE_ENDPOINT}/${id}`);
+};
+
+const createData = async () => {
+  const product = {
+    name: "Apple MacBook Pro 2020",
+    data: {
+      year: 2020,
+      price: 1999.99,
+      "CPU model": "M1 pro",
+      "Hard disk size": "1 TB",
+      color: "space gray",
+    },
+  };
+
+  const response = await request.post(BASE_ENDPOINT, product);
+  return await response.json();
+};
+
+describe("Demo for the http client", () => {
+  beforeEach(async () => {
+    if (preparedData) await cleanData(preparedData.id);
+    preparedData = await createData();
+  });
+
+  afterAll(async () => {
+    if (preparedData) await cleanData(preparedData.id);
+  });
+
+  test("Check get() method to get single object", async () => {
+    const response = await request.get(`${BASE_ENDPOINT}/${preparedData.id}`);
+    expect(response.status).toBeEqual(200);
+  });
+
+  test("Check get() method to get all objects", async () => {
+    const response = await request.get(BASE_ENDPOINT);
     const data = await response.json();
-    expect(data.forms[0].name).toBeEqual("ditto");
-    expect(data.id).toBeEqual(132);
+    expect(response.status).toBeEqual(200);
+    expect(data.length).toBeGreaterThan(1);
+  });
+
+  test("Check put() method to update single object", async () => {
+    const product = {
+      name: "Apple MacBook Pro 2021",
+      data: {
+        year: 2021,
+        price: 1999.99,
+        "CPU model": "M2 pro",
+        "Hard disk size": "1 TB",
+        color: "silver",
+      },
+    };
+    const response = await request.put(
+      `${BASE_ENDPOINT}/${preparedData.id}`,
+      product
+    );
+    const data = await response.json();
+    expect(response.status).toBeEqual(200);
+    expect(data.updatedAt).toBeDefined();
+    expect(data.id).toBeEqual(preparedData.id);
+    expect(data.name).toBeEqual(product.name);
+  });
+
+  test("Check patch() method to update single object", async () => {
+    const product = {
+      name: "Apple MacBook Air",
+    };
+    const response = await request.patch(
+      `${BASE_ENDPOINT}/${preparedData.id}`,
+      product
+    );
+    const data = await response.json();
+    expect(response.status).toBeEqual(200);
+    expect(data.updatedAt).toBeDefined();
+    expect(data.id).toBeEqual(preparedData.id);
+    expect(data.name).toBeEqual(product.name);
   });
 });
